c66b443cf006b260a234094f2c0acc0672bbfcf7
Polastrine <diogo.dsilva@sptech.school>
2024-06-05 03:23:29 -0300

    Integração da tela de cadastro com a API

--- SiteOficial/src/controllers/usuarioController.js
+++ SiteOficial/src/controllers/usuarioController.js
@@ -2,8 +2,8 @@ var usuarioModel = require("../models/usuarioModel");
 var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
+    let email = req.body.emailServer;
+    let senha = req.body.senhaServer
 
     if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
@@ -15,25 +15,21 @@ function autenticar(req, res) {
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                        res.json({
+                            id: resultadoAutenticar[0].id,
+                            nome: resultadoAutenticar[0].nome,
+                            email: resultadoAutenticar[0].email,
+                            cpf: resultadoAutenticar[0].cpf,
+                            telFixo: resultadoAutenticar[0].telFixo,
+                            telCel: resultadoAutenticar[0].telCel,
+                            empresa: resultadoAutenticar[0].idEmpresa
+                        });
+
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -52,25 +48,28 @@ function autenticar(req, res) {
 }
 
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
-    var empresaId = req.body.empresaServer;
+    let nome = req.body.nomeServer
+    let email = req.body.emailServer;
+    let cpf = req.body.cpfServer;
+    let telFixo = req.body.telFixoServer;
+    let telCel = req.body.telCelServer;
+    let idEmpresa = req.body.idEmpresaServer;
 
-    // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (empresaId == undefined) {
-        res.status(400).send("Sua empresa está undefined!");
+        res.status(400).send("Seu email está indefinido!");
+    } else if (cpf == undefined) {
+        res.status(400).send("Seu cpf está indefinido!");
+    } else if (telFixo == undefined) {
+        res.status(400).send("Seu telefone fixo está indefinido!");
+    } else if (telCel == undefined) {
+        res.status(400).send("Seu telefone celular está indefinido!");
+    } else if (idEmpresa == undefined) {
+        res.status(400).send("Sua empresa está indefinido!");
     } else {
 
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, empresaId)
+        usuarioModel.cadastrar(cpf, nome, email, telFixo, telCel, idEmpresa)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- SiteOficial/src/models/usuarioModel.js
+++ SiteOficial/src/models/usuarioModel.js
@@ -9,14 +9,11 @@ function autenticar(email, senha) {
     return database.executar(instrucaoSql);
 }
 
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, CPF, email, senha, idEmpresa, idTipoUsuario) {
+function cadastrar(CPF, Nome, Email, TelFixo, TelCelular, idEmpresa) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO Usuario (nome, CPF, email, senha, telFixo, telCelular, fkEmpresa, fkTipoUsuario) VALUES ('${nome}', '${CPF} '${email}', '${senha}', '${idEmpresa}', '${idTipoUsuario}');
+        INSERT INTO Usuario (CPF, Nome, E-mail, Senha, TelFixo, TelCelular, fkEmpresa, DataCriacao) VALUES ('${CPF}', '${Nome} '${Email}', '${idEmpresa+CPF}', ${TelFixo}, '${TelCelular}', '${idEmpresa}', now());
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
