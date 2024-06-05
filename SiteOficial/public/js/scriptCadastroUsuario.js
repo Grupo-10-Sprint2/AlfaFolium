e19343f57b8c360ee2153c4c7cf115e93285d9ae
Polastrine <diogo.dsilva@sptech.school>
2024-06-04 22:54:23 -0300

    Finalização das Telas de Cadastro

---
+++ SiteOficial/public/js/scriptCadastroUsuario.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function transicaoCadastro() {
+    let boxCadastro = document.querySelector('.boxCadastro')
+
+    boxCadastro.style.transform = 'translateY(-0.1%)'
+    boxCadastro.style.transition = '1.2s'
+});
