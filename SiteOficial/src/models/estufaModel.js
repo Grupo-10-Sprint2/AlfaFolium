c5778899202ec556ae2ee4c89a30fa797e7eef03
Polastrine <diogo.dsilva@sptech.school>
2024-06-06 21:26:07 -0300

    Telas de Cadastro integradas à API

--- SiteOficial/src/models/estufaModel.js
+++ SiteOficial/src/models/estufaModel.js
@@ -1,7 +1,24 @@
 var database = require("../database/config");
 
+function cadastrar(tamanhoEstufa, statusEstufa, idEmpresa, TempMaxima, TempMinima, UmidMaxima, UmidMinima) {
+    var instrucaoSql = `
+    INSERT INTO parametro (umidadeMin, umidadeMax, temperaturaMin, temperaturaMax) 
+    VALUES (${UmidMinima}, ${UmidMaxima}, ${TempMinima}, ${TempMaxima});
+`;
 
+    return database.executar(instrucaoSql)
+        .then(result => {
+            var idParametro = result.insertId;
+            var instrucaoSql = `
+            INSERT INTO estufa (tamanho, status, fkEmpresa, fkParametro) 
+            VALUES (${tamanhoEstufa}, '${statusEstufa}', ${idEmpresa}, ${idParametro});
+        `;
+            return database.executar(instrucaoSql);
+        });
+
+}
 
 
 module.exports = {
+    cadastrar
 }
